8337a022b91829fd8ba991a93be5cef15ff988f5
takanori.azegami <takanori.azegami.ta@ultmarc.jp>
2023-07-14 18:10:55 +0900

    update

--- client/src/lib/apiClient.ts
+++ client/src/lib/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+	baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     'Content-Type': 'application/json',
   },
